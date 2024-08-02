2dac02aaf654b2ca54e324184a6d6da186051184
oWcAeLfIses <95765231+oWcAeLfIses@users.noreply.github.com>
2024-08-02 21:33:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import random
 import string
 st.title('Generator hasel ')
-st.button('Generuj'):
 if st.button('Generuj'):
     def generate_password(length=16):
     znaki = string.ascii_letters + string.digits + string.punctuation
