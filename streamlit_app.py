4cfa26e8b0fa8657629d3927776f3c3f5c55a335
oWcAeLfIses <95765231+oWcAeLfIses@users.noreply.github.com>
2024-08-02 21:34:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import string
 st.title('Generator hasel ')
 if st.button('Generuj'):
     def generate_password(length=16):
-    znaki = string.ascii_letters + string.digits + string.punctuation
-    haslo = ''.join(random.choice(characters) for _ in range(length))
-    return password
+        znaki = string.ascii_letters + string.digits + string.punctuation
+        haslo = ''.join(random.choice(characters) for _ in range(length))
+        return password
     print(generate_password())
